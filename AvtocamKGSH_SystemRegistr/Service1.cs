a953a26cc5ecc7756e225b98a6b790e2a746e45d
nikonovichoa <nikonovichoa@C0012106>
2023-04-17 15:15:15 +0300

    Добавьте файлы проекта.

---
+++ AvtocamKGSH_SystemRegistr/Service1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using OwenProtocol.IO;
+using OwenProtocol;
+using System.Threading;
+using static System.Net.WebRequestMethods;
+
+namespace AvtocamKGSH_SystemRegistr
+{
+    public partial class Service1 : ServiceBase
+    {
+        SerialPortAdapter sp = new SerialPortAdapter(3, 115200, 0, 8, System.IO.Ports.StopBits.One);
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+              try
+                {
+                   Task.Factory.StartNew(() =>
+                   {
+                       if (!sp.IsOpened) sp.Open();
+                       doWhile();
+
+                   });
+                 System.IO.File.AppendAllText("d:\\" + "ошибка.txt", "OnStart1 " +  "\n");
+ 
+                }
+                catch (Exception ex)
+                {
+                    System.IO.File.AppendAllText("d:\\" + "ошибка.txt",  "OnStart - " + ex.Message + "\n");
+                } 
+        }
+
+        public void doWhile()
+        {
+            byte[] res;
+            float value;
+            System.IO.File.AppendAllText("d:\\" + "ошибка.txt", " doWhile start" + "\n");
+            OwenioNet.DataConverter.Converter.ConverterFloat conv = new OwenioNet.DataConverter.Converter.ConverterFloat(3);
+            using (var OPM = OwenProtocolMaster.Create(sp))
+            {
+                try
+                {
+                while (true)
+                    {
+                        WorkWithData.ReadDevices();
+                        if (WorkWithData.DevicesReader.HasRows)
+                        {
+                            while (WorkWithData.DevicesReader.Read())
+                            {
+                               Int16 adr =  Convert.ToInt16(WorkWithData.DevicesReader[0]);
+                                try 
+                                { 
+                                    res = OPM.OwenRead(adr, OwenProtocol.Types.AddressLengthType.Bits8, "PV");
+                                    value = conv.ConvertBack(res);
+                                }
+                                catch (Exception ex)
+                                {   
+                                    value = 0;
+                                    WorkWithData.InsertValue(adr, value);
+                                    System.IO.File.AppendAllText("d:\\" + "ошибка.txt", " ошибка чтения " + adr.ToString() + ex.Message +"\n");
+                                    if (!sp.IsOpened) sp.Open();
+                                    continue; 
+                                }
+                                WorkWithData.InsertValue(adr, value);
+                                //Thread.Sleep(1000);
+                            }
+                        }
+                        WorkWithData.DevicesReader.Close();
+                        Thread.Sleep(5000);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.IO.File.AppendAllText("d:\\" + "ошибка.txt", " ошибка doWhile " + ex.Message + "\n");
+                    if (!sp.IsOpened) sp.Open();
+                    doWhile();
+                }
+            }
+        }
+
+        protected override void OnStop()
+        {
+           if (!WorkWithData.DevicesReader.IsClosed) WorkWithData.DevicesReader.Close();
+           if (sp.IsOpened) sp.Close();
+         }
+    }
+}
---
+++ AvtocamKGSH_SystemRegistr/WorkWithData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvtocamKGSH_SystemRegistr
+{
+    internal class WorkWithData
+    {
+        private static SqlConnection connection { get; } = new SqlConnection(Properties.Settings.Default.KGSH_Avtocam_RegistrConnectionString);
+        private static SqlConnection connection_1 { get; } = new SqlConnection(Properties.Settings.Default.KGSH_Avtocam_RegistrConnectionString);
+        public static SqlDataReader DevicesReader = null;
+        private static string sqlExpression;
+
+        // выбираем записи по одному ID 
+        static public  void InsertValue(Int16 adr, float temp)
+        {
+            
+                sqlExpression = "InsertTemperature";
+                DateTime curTime = DateTime.Now;
+                using (SqlCommand insert_command = new SqlCommand(sqlExpression, connection))
+                {
+                    if (connection.State == 0) connection.Open();
+                    // указываем, что команда представляет хранимую процедуру
+                    insert_command.CommandType = System.Data.CommandType.StoredProcedure;
+                    // параметр для ввода имени
+                    insert_command.Parameters.AddWithValue("adr", adr);
+                    insert_command.Parameters.AddWithValue("temp", temp);
+                    insert_command.Parameters.AddWithValue("currtime", curTime);
+                    insert_command.ExecuteNonQuery();
+                }
+        }
+
+        public  static void ReadDevices()
+        {
+            if (connection_1.State == 0) connection_1.Open();
+            string sqlExpression = "SELECT modbus_adress from Devices";
+            SqlCommand read_command = new SqlCommand(sqlExpression, connection_1);
+            DevicesReader = read_command.ExecuteReader();
+        }
+    }
+}
